aa4aa1053a951109a4e828d216bff041735dc878
Ilana T <ilana.tordjman@edu.ece.fr>
2019-05-30 16:17:37 +0200

    Modif de classes

--- ProjetJava/src/Modele/Classe.java
+++ ProjetJava/src/Modele/Classe.java
@@ -2,4 +2,24 @@ package Modele;
 
 public class Classe {
     
+    private String nom;
+    private AnneeScolaire annee;
+    private Ecole ecole;
+    private Niveau niveau;
+    
+    public AnneeScolaire getAnneeScolaire(){
+        return this.annee;
+    }
+    
+    public Ecole getEcole(){
+        return this.ecole;
+    }
+    
+    public Niveau getNiveau(){
+        return this.niveau;
+    }
+    
+    public String getNom(){
+        return this.nom;
+    }
 }
--- ProjetJava/src/Modele/Trimestre.java
+++ ProjetJava/src/Modele/Trimestre.java
@@ -2,4 +2,19 @@ package Modele;
 
 public class Trimestre {
     
+    private AnneeScolaire annee;
+    
+    public Trimestre(){
+        this.annee= null;
+    }
+    
+    public Trimestre(AnneeScolaire a){
+        
+        this.annee=a;
+    }
+    
+    public AnneeScolaire getAnneeScolaire(){
+        return this.annee;
+    }
+    
 }
